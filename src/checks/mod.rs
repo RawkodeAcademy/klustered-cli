74f50d3df1042147f21be562b78e631cfd485dfd
Andrew Welker <awelker@pepperdash.com>
2022-08-14 22:02:28 -0600

    feat: add server-client version match check

--- src/checks/mod.rs
+++ src/checks/mod.rs
@@ -1,2 +1,3 @@
 pub mod cli_version;
 pub mod server_version;
+pub mod version_match;
--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,11 @@ fn main() {
         Err(e) => panic!("❌ Failed to check kubectl client version: {}", e),
     };
 
+    println!(
+        "✅ kubectl version {}.{}",
+        cli_version.client_version.major, cli_version.client_version.minor
+    );
+
     let server_version = match checks::server_version::check() {
         Ok(c) => c,
         Err(e) => panic!("❌ Failed to check kubectl server version: {}", e),
@@ -13,11 +18,10 @@ fn main() {
 
     println!(
         "✅ kubectl version {}.{}",
-        cli_version.client_version.major, cli_version.client_version.minor
-    );
-
-    println!(
-        "✅ kubectl version {}.{}",
         server_version.server_version.major, server_version.server_version.minor
     );
+
+    let version_match:&str= if checks::version_match::check(cli_version.client_version, server_version.server_version) {"✅"} else {"❌"};
+
+    println!("{} client and server versions match", version_match);
 }
